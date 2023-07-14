ee6a541f9699624bb837b537c0afe8df022924a5
Loveesh Singh <loveesh.20132@gmail.com>
2023-07-14 21:57:10 +0530

    comments added 2

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,7 @@
 public class hello_world{
     public static void main(String[] args){
         System.out.println("hello world");
-        //commenting
+        //single line commenting
         /**
          * multi
          * line
