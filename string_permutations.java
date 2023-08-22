331669ee397c022fdad67a0a83de6a7f7838b5dd
Loveesh Singh <loveesh.20132@gmail.com>
2023-08-22 23:36:35 +0530

    tried

---
+++ string_permutations.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class string_permutations{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = scn.next();
+        for (int i=0 ; i<str.length() ; i++ ){
+            
+        }
+        
+    }
+}
