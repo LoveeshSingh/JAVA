89f592253c2425cca2aa36f5c6777a39619c7a4e
Loveesh Singh <loveesh.20132@gmail.com>
2023-10-12 14:15:35 +0530

    subsequence and dialpad

--- recursion/print_subsequence.java
+++ recursion/print_subsequence.java
@@ -5,27 +5,21 @@ public class print_subsequence{
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter string: ");
         String str = scn.next();
-        ArrayList<String> list = ArrayList<>();
-        subsequence(list,str,copy)
+        ArrayList<String> list = subsequence(str);
+        System.out.println(list);
     }
 
-    public static void swap(ArrayList<String> list , String str, int index){
-        int num = arr[i];
-        arr[i]=arr[j];
-        arr[j]=num;
-    }
-
-    public static void subsequence(int[] arr,int index1,int index2){
-        if (index1==index2-1){
-            System.out.print(arr[index1]);
-            return;
+    public static ArrayList<String> subsequence(String str){
+        if (str==""){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(" ");
+            return list;
         }
-        for (int i=index1 ; i<index2-1 ; i++){
-            System.out.print(arr[i]+" ");
-            swap(arr,index1,i);
-            subsequence(arr,index1+1,index2);
-            swap(arr,index1,i);
-            System.out.println();
+        ArrayList<String> list = subsequence((str.length()>1)?str.substring(1):"");
+        int len = list.size();
+        for (int i=0 ; i<len ; i++){
+            list.add(str.charAt(0)+list.get(i));
         }
+        return list;
     }
 }
