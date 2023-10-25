008ab805e2131705e5c90cc2e5c112130b2e2b54
Loveesh Singh <loveesh.20132@gmail.com>
2023-10-25 21:19:43 +0530

    print permutation and combination of coin change

--- recursion/n_queens.java
+++ recursion/n_queens.java
@@ -2,8 +2,59 @@ import java.util.*;
 
 public class n_queens{
     public static void main(String[] args){
-        
+        //chess should be false boolean here 8x8 chess is used
+        boolean[][] chess =  {
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false},
+            {false,false,false,false,false,false,false,false}
+        };
+        isQueenSafe(chess,9,0,"");
     }
 
-    public static 
+    public static void isQueenSafe(boolean[][] chess , int queens ,int column,String str){
+        if (queens==0){
+            System.out.println(str);
+            return;
+        }
+        else if (column>=chess[0].length){
+            return;
+        }
+        for (int i=0 ; i<chess.length ; i++ ){
+            int flag=0;
+            // checking west side
+            for (int j=0 ; j<column ; j++){
+                if (chess[i][j]){
+                    flag=1;
+                    break;
+                }
+            }
+
+            //checking north west side
+            for (int r=i,c=column ; ((r>=0)&&(c>=0)) ; r--,c--){
+                if (chess[r][c]){
+                    flag=1;
+                    break;
+                }
+            }
+
+            //checking south west side
+            for (int r=i,c=column ; ((r<chess.length)&&(c>=0)) ; r++,c--){
+                if (chess[r][c]){
+                    flag=1;
+                    break;
+                }
+            }
+
+            if (flag==0){
+                chess[i][column]=true;
+                isQueenSafe(chess , queens-1 , column+1 ,str+Integer.toString(i)+Integer.toString(column)+" || " );
+                chess[i][column]=false;
+            }
+        }
+    }
 }
